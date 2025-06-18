78468145f073bc8aa315206fbd9b46b92e37ea45
littleblack111 <littleblack11111@gmail.com>
2025-06-18 09:17:54 +0800

    fix: disallow the creation of multiple servers
    style: errors with the class information

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -3,6 +3,7 @@
 #include "misc/FileDescriptor.hpp"
 #include <cstring>
 #include <mutex>
+#include <stdexcept>
 #include <sys/socket.h>
 
 using namespace LibSock::Server;
@@ -11,19 +12,22 @@ Server::Server(uint16_t port, bool reuseaddr, bool keepalive)
 	: m_sockfd(
 		  std::make_shared<LibSock::CFileDescriptor>(socket(AF_INET, SOCK_STREAM, 0))
 	  ) {
-	std::lock_guard<std::mutex> lk(m_mutex);
+	if (pServer)
+		throw std::runtime_error("server:server Server already exist");
+	std::lock_guard<std::mutex>
+		lk(m_mutex);
 	if (!m_sockfd->isValid() || m_sockfd->get() < 0)
-		throw std::runtime_error("Failed to create socket");
+		throw std::runtime_error("server:server Failed to create socket");
 
 	constexpr int opt  = 1;
 	constexpr int size = sizeof(opt);
 	if (keepalive &&
 		setsockopt(m_sockfd->get(), SOL_SOCKET, SO_REUSEADDR, &opt, size) < 0)
-		throw std::runtime_error("setsockopt(SO_REUSEADDR) failed");
+		throw std::runtime_error("server:server setsockopt(SO_REUSEADDR) failed");
 
 	if (reuseaddr &&
 		setsockopt(m_sockfd->get(), SOL_SOCKET, SO_KEEPALIVE, &opt, size) < 0)
-		throw std::runtime_error("setsockopt(SO_KEEPALIVE) failed");
+		throw std::runtime_error("server:server setsockopt(SO_KEEPALIVE) failed");
 
 	memset(&m_addr, 0, sizeof(m_addr));
 	m_addr.sin_family	   = AF_INET;
@@ -31,7 +35,7 @@ Server::Server(uint16_t port, bool reuseaddr, bool keepalive)
 	m_addr.sin_addr.s_addr = INADDR_ANY;
 	if (bind(m_sockfd->get(), reinterpret_cast<sockaddr *>(&m_addr), sizeof(m_addr)) ||
 		listen(m_sockfd->get(), 5))
-		throw std::runtime_error("Failed to bind or listen on socket");
+		throw std::runtime_error("server:server Failed to bind or listen on socket");
 
 	pServer = SP<Server>(this);
 };
