68f3b0a0a95f6cb458729cbe8d1ffa8dde5220bf
littleblack111 <littleblack11111@gmail.com>
2025-06-23 12:33:35 +0800

    API(then): allow using operator |

--- include/libsock/misc/then.hpp
+++ include/libsock/misc/then.hpp
@@ -1,5 +1,6 @@
 #include <future>
 #include <type_traits>
+#include <utility>
 
 namespace LibSock {
 template <typename T, typename Func>
@@ -15,5 +16,25 @@ auto then(std::future<void> fut, Func func) -> std::future<std::invoke_result_t<
 		fut.wait();
 		return func();
 	});
+}
+
+template <typename Func>
+struct Then {
+	Func func;
+};
+
+template <typename Func>
+Then<Func> then(Func f) {
+	return Then<Func>{std::move(f)};
+}
+
+template <typename T, typename Func>
+auto operator|(std::future<T> fut, Then<Func> thenObj) -> std::future<std::invoke_result_t<Func, T>> {
+	return then(std::move(fut), std::move(thenObj.func));
+}
+
+template <typename Func>
+auto operator|(std::future<void> fut, Then<Func> thenObj) -> std::future<std::invoke_result_t<Func>> {
+	return then(std::move(fut), std::move(thenObj.func));
 }
 } // namespace LibSock
